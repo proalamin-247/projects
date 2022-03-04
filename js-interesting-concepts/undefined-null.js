d6b7dbbf07cc5fd91eaf6fdd6cc6f84f1059bafd
proalamin-247 <pro.alamin247@gmail.com>
2022-03-04 11:14:31 +0600

    35 module start 4now video runing

---
+++ js-interesting-concepts/undefined-null.js
@@ -0,0 +1,14 @@
+/*
+variable value not assigned
+function but didn't write return anything
+
+*/
+
+let first;
+// console.log(first);
+
+function second(x, y){
+    const sum = x+ y;
+}
+const result = second(2, 3);
+console.log(result);
